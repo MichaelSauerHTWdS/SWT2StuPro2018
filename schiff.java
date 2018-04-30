5a71bed65e781a21b00c7a424cc533ae9ed191b5
mlangenfeld <berni1994@web.de>
2018-04-30 11:30:33 +0200

    0.111

--- schiff.java
+++ schiff.java
@@ -24,21 +24,44 @@ public class Schiff
 		switch (typ) 
 		{
 		case 1: this.typname = "Schnigge";
+				this.maxlad = 40;
 				break;
 		case 2:	this.typname = "Kogge";
+				this.maxlad = 100;
 				break;
 		case 3:	this.typname = "Holk";
+				this.maxlad = 150;
 				break;
 		case 4:	this.typname = "Kraweel";
+				this.maxlad = 400;
 				break;
 		case 5:	this.typname = "Karacke";
+				this.maxlad = 600;
 				break;
 		case 6:	this.typname = "Linienschiff";
+				this.maxlad = 1000;
 				break;
 		default:
 				break;
 		}
 		antwort = this.name + " ist jetzt eine " + this.typname;
+		
+		return antwort;
+	}
+	
+	public String beladen(int anzahl) 
+	{
+		String antwort;
+		
+		if (anzahl + this.ladung <= maxlad) 
+		{
+			this.ladung = this.ladung + anzahl;
+			antwort = "Beladung erfolgreich. Es sind noch " + (this.maxlad - this.ladung) + " Einheiten frei.";
+		}
+		else
+		{
+			antwort = "Nicht genuegend Platz";
+		}
 		
 		return antwort;
 	}
