a51a54038cacd69813ef7820866158997ad14af0
Benni0x0 <38464668+Benni0x0@users.noreply.github.com>
2018-05-14 12:59:51 +0200

    Add files via upload

--- src/Kaufmann.java
+++ src/Kaufmann.java
@@ -6,70 +6,73 @@
  */
 import java.util.Scanner;
 
+
+
 public class Kaufmann 
 {
 	private String name;
 	private String prename;
-	private double vermoegen;
-
-
 	
-	
+
 	
 	/**
 	 * Konstruktor der Kaufmann Klasse.
 	 */
-	public Kaufmann ()
+	public Kaufmann () throws Exception
 	{
 		Scanner scan = new Scanner(System.in);
 
 		/**
 		 * Es wird geprüft, ob die Eingaben nicht leer sind und dann erst in den Attribute geschrieben.
 		 */
-		try {
-
+	
 			System.out.println("Bitte geben Sie den Vornamen ein: \n");
 			prename = scan.nextLine();
 			if (prename.isEmpty() == true)
 			{
 				throw new Exception("Der Vorname darf nicht leer sein!\n");
-			}				
-			System.out.println("Bitte geben Sie den Nachnamen ein: \n");
-		
+			}	
+			
+			System.out.println("Bitte geben Sie den Nachnamen ein: \n");		
 			name =scan.nextLine();				
 			if (name.isEmpty() == true)
 			{
 				throw new Exception("Der Vorname darf nicht leer sein!\n");
 			}
 			
-			System.out.println("Bitte geben Sie das Vermoegen des Kaufmanns ein: \n");
+			Konto konto1 = new Konto();
+			
+		/*	System.out.println("Bitte geben Sie das Vermoegen des Kaufmanns ein: \n");
 			vermoegen = scan.nextDouble();
 			if (vermoegen <= 0.0)
 			{
 				throw new Exception("Das Vermoegen darf nicht negativ oder 0 sein\n");
 			}
+			*/
 			
-		}
-	catch (Exception e)
-	{
-		System.out.println(e.getMessage());
+			
+			
 	}
-		
-	}
-	
 	public void print() 
 	{
-		System.out.println("Name: " + prename + " "+  name + "\n Vermoegen: " + vermoegen);
+		System.out.println("Name: " + prename + " "+  name + "\n Vermoegen: " + );
 		
 	}
 	
 	public static void main( String[] args)
 	{
-
-		Kaufmann k = new Kaufmann();
-
-		k.print();
-
-	}
-
+		try 
+		{
+			Kaufmann k = new Kaufmann();
+			k.print();
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}	
 }
+
+
