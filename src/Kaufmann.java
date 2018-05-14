12b4c1f04e47a8ccab34c1e3b9d2ce44459ca122
Benni0x0 <38464668+Benni0x0@users.noreply.github.com>
2018-05-14 11:27:18 +0200

    Exception verbessert

--- src/Kaufmann.java
+++ src/Kaufmann.java
@@ -11,52 +11,65 @@ public class Kaufmann
 	private String name;
 	private String prename;
 	private double vermoegen;
+
+
 	
 	
 	
 	/**
 	 * Konstruktor der Kaufmann Klasse.
 	 */
-	public Kaufmann (String name, String prename)
+	public Kaufmann ()
 	{
+		Scanner scan = new Scanner(System.in);
+
+		/**
+		 * Es wird geprüft, ob die Eingaben nicht leer sind und dann erst in den Attribute geschrieben.
+		 */
 		try {
-			if (name == "")
+
+			System.out.println("Bitte geben Sie den Vornamen ein: \n");
+			prename = scan.nextLine();
+			if (prename.isEmpty() == true)
 			{
-				throw new Exception("Der Name darf nicht leer sein!\n");
-			}
-			this.name = name;
-			if (prename == "")
+				throw new Exception("Der Vorname darf nicht leer sein!\n");
+			}				
+			System.out.println("Bitte geben Sie den Nachnamen ein: \n");
+		
+			name =scan.nextLine();				
+			if (name.isEmpty() == true)
 			{
 				throw new Exception("Der Vorname darf nicht leer sein!\n");
 			}
-			this.prename = prename;
-			vermoegen = 0.0;
+			
+			System.out.println("Bitte geben Sie das Vermoegen des Kaufmanns ein: \n");
+			vermoegen = scan.nextDouble();
+			if (vermoegen <= 0.0)
+			{
+				throw new Exception("Das Vermoegen darf nicht negativ oder 0 sein\n");
+			}
+			
 		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
+	catch (Exception e)
+	{
+		System.out.println(e.getMessage());
+	}
 		
 	}
 	
 	public void print() 
 	{
-		System.out.println("Name: " + prename + " "+  name);
+		System.out.println("Name: " + prename + " "+  name + "\n Vermoegen: " + vermoegen);
 		
 	}
 	
 	public static void main( String[] args)
 	{
-		Scanner scan = new Scanner(System.in);
 
-		Kaufmann k = new Kaufmann("Mustermann" , "Max");
-		
-		System.out.println("Bitte geben Sie den Vornamen ein: \n");
-		k.prename = scan.nextLine();
-		System.out.println("Bitte geben Sie den Nachnamen ein: \n");
-		k.name =scan.nextLine();
-		
+		Kaufmann k = new Kaufmann();
+
 		k.print();
+
 	}
-	
+
 }
