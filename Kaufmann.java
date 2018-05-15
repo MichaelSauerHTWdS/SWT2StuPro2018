7ebddc64dbf1f477a4782300dcc3bb1d978cb4dd
Benni0x0 <38464668+Benni0x0@users.noreply.github.com>
2018-05-15 08:24:17 +0200

    Add files via upload

---
+++ Kaufmann.java
@@ -0,0 +1,70 @@
+
+/**
+ * 
+ * @author Benjamin Hesser, Marvin Backes
+ *
+ */
+import java.util.Scanner;
+
+
+
+public class Kaufmann 
+{
+	private String name;
+	private String prename;
+	Konto konto1 = null;
+	
+
+	
+	/**
+	 * Konstruktor der Kaufmann Klasse.
+	 */
+	public Kaufmann () throws Exception
+	{
+		Scanner scan = new Scanner(System.in);
+
+		/**
+		 * Es wird geprüft, ob die Eingaben nicht leer sind und dann erst in den Attribute geschrieben.
+		 */
+	
+			System.out.println("Bitte geben Sie den Vornamen ein: \n");
+			prename = scan.nextLine();
+			if (prename.isEmpty() == true)
+			{
+				throw new Exception("Der Vorname darf nicht leer sein!\n");
+			}	
+			
+			System.out.println("Bitte geben Sie den Nachnamen ein: \n");		
+			name =scan.nextLine();				
+			if (name.isEmpty() == true)
+			{
+				throw new Exception("Der Vorname darf nicht leer sein!\n");
+			}
+			
+			konto1 = new Konto();
+	
+			
+	}
+	public void print() 
+	{
+		System.out.println("Name: " + prename + " "+  name + "\n Vermoegen: " + konto1 );
+		
+	}
+	
+	public static void main( String[] args)
+	{
+		try 
+		{
+			Kaufmann k = new Kaufmann();
+			k.print();
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}	
+}
+
+
