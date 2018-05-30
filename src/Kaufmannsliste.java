b7ffce101706f556f7bf6effd6a0d4c2b421db4e
jpinnecker <jpinnecker@htwsaar.de>
2018-05-30 10:32:15 +0200

    Updated: Kaufmannsliste hat nun ein Attribut weniger. Die Anzahl an Kaufmaennern wird nun ueber das Array Attribut "length" ermittelt.

--- src/Kaufmannsliste.java
+++ src/Kaufmannsliste.java
@@ -4,23 +4,21 @@
  *
  * @date 28.05.18
  * @author Joshua Pinnecker
- * @version 0.1
+ * @version 1
  */
 class Kaufmannsliste{
 	static private final String ILLEGAL_SPIELERZAHL = "Die Anzahl teilnehmender Spieler muss 2, 3 oder 4 sein.";
 	static private final int MAX_SPIELERZAHL = 4;
 	private Kaufmann[] liste;
-	private final int spielerzahl;
 
 	public Kaufmannsliste(int spielerzahl){
 		if (spielerzahl > MAX_SPIELERZAHL || spielerzahl < 2)
 			throw new IllegalArgumentException(ILLEGAL_SPIELERZAHL);
 		liste = new Kaufmann[spielerzahl];
-		this.spielerzahl = spielerzahl;
 		}
 
 	public int getSpielerzahl(){
-		return spielerzahl;
+		return liste.length;
 	}
 }
 	
